6a6fb683508f820ed1d3ae68d40e88fd44410ff6
Zevenberge <nanayamae@gmail.com>
2017-08-22 22:48:17 +0200

    Set up of the project using dub's subprojects

---
+++ domain/source/element.d
@@ -0,0 +1,9 @@
+module sliding.domain.element;
+
+abstract class Element
+{
+	protected Element leftNeighbour;
+	protected Element topNeighbour;
+	protected Element rightNeighbour;
+	protected Element bottomNeighbour;
+}
---
+++ source/lib.d
@@ -0,0 +1,2 @@
+﻿module sliding.lib;
+
