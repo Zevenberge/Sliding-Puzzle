d40ad28718f15ab5811be9246adda81b4b3b2eba
Marco de Wild <mdwild@sogyo.nl>
2017-09-06 16:03:24 +0200

    Keyboard controls geïmplementeerd

--- source/sliding/ui/controls.d
+++ source/sliding/ui/controls.d
@@ -1,9 +1,49 @@
 ﻿module sliding.ui.controls;
 
 import dsfml.graphics;
+import sliding.domain.avoid;
+import sliding.domain.direction;
 
 interface Control
 {
 	bool handle(Event event);
 }
 
+class KeyboardControl : Control
+{
+	this(Void void_)
+	{
+		_void = void_;
+	}
+
+	private Void _void;
+
+	bool handle(Event event)
+	{
+		if(event.type == Event.EventType.KeyPressed)
+		{
+			return handleKey(event.key.code);
+		}
+		return false;
+	}
+
+	private bool handleKey(Keyboard.Key key)
+	{
+		switch(key) with (Keyboard.Key)
+		{
+			case Left:
+				_void.move(Direction.left);
+				return true;
+			case Up:
+				_void.move(Direction.top);
+				return true;
+			case Right:
+				_void.move(Direction.right);
+				return true;
+			case Down:
+				_void.move(Direction.bottom);
+				return true;
+		}
+		return false;
+	}
+}
